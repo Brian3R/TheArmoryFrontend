9a66a7aff9bb36e5448c87515254b96d8336657f
Owen Ziegler <odz2@txstate.edu>
2023-04-19 13:54:19 -0500

    added signup warning

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,20 +10,20 @@ const Home = () => {
                 <h1 className='text'>
                     Home!
                 </h1>
-                <p className='text'>
+                <p className='text' style={{width:'600px'}}>
                     Welcome to the Armory. Please sign up or login to access the inventory, new item screen, and generator.
                 </p>
                 <h2 className='text'>
                     Get Started
                 </h2>
-                <p style={{width:'500px'}}className='text'>
+                <p style={{width:'600px'}}className='text'>
                     To begin, please click Sign UP to create an account. Once your account is created, add some items to your
                     inventory using the Add Item page. Once your inventory has been filled, use the Generator to recieve outfits.
                 </p>
                 <h2 className='text'>
                     Background
                 </h2>
-                <p style={{width:'500px'}}className='text'>
+                <p style={{width:'600px'}}className='text'>
                     Our algorithm is based on two data points: Colors and favorability scores. First, we choose the top for the outfit
                     based on favorability scores, then we choose the pants and shoes based on favorability, how they match with each other,
                     and how they match with the already-selected shirt. We take feedback from you when you 'like' and 'dislike' outfits,
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -10,6 +10,10 @@ const SignUp = () => {
                 <h1 className='text'>
                     Sign Up
                 </h1>
+                <p className='text' style={{width:'400px'}}>
+                    WARNING: This website is a class project, and as such, does not have robust account security.
+                    Please do not use any password that you use for other websites.
+                </p>
                 <SignUpForm/>
             </div>
             <br/>
